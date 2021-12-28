ea56f0c388c69336a80cad12d37e4d5225703452
dzakialhafez <dzakialhafiz93@gmail.com>
2021-12-28 16:37:25 +0700

    baru

---
+++ alert.php
@@ -0,0 +1,19 @@
+   <html>  
+    <head>  
+    <title> Fitur Notifikasi </title>  
+   <meta name="viewport" content="width=device-width, initial-scale=1">  
+    <meta charset ="utf-8">  
+    <style>  
+    body {  
+        background-color: #fff;  
+    }  
+    </style>  
+    <?php   
+       echo '<script type ="text/JavaScript">';  
+       echo 'alert("Yuhuuu, ini fitur notifikasi guys")';  
+       echo '</script>';  
+   ?>  
+    </head>  
+    <body>  
+    </body>  
+    </html>  
